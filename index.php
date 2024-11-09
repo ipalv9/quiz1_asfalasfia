ac529c18d34c98f2bc7243a7f437fdfafd90b894
ipalv9 <ipal7271@gmail.com>
2024-11-09 10:32:17 +0700

    membuat form dosen

--- index.php
+++ index.php
@@ -43,6 +43,55 @@
     </div>
   </div>
 </nav>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+            <div class="card-header">
+                <h3>Formulir Dosen</h3>
+            </div>
+            <div class="card-body">
+            <form action="proses.php" method="POST" enctype="multipart/form-data">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">NIDN</label>
+    <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nama Lengkap</label>
+    <input type="text" name="tempat" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Rumpun</label>
+        <select name="rumpun" id="" class="form-control">
+            <option value="">-Pilih Rumpun-</option>
+            <option value="Islam">Komputer</option>
+            <option value="Kristen">Akuntansi</option>
+            <option value="Hindu">Manajemen</option>
+            <option value="Katolik">Perpajakan</option>
+            <option value="Buddha">Bisnis/option>
+            </select>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Tempat Lahir</label>
+    <input type="text" name="tempat" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Tanggal Lahir</label>
+    <input type="date" name="tanggal" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Email</label>
+    <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+  </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="reset" class="btn btn-primary">Reset</button>
+</form>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
 
 
 <script src="js/bootstrap.js"></script>
