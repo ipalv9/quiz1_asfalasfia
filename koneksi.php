3fdcbe6f438c13cc7025d5829208e2cf4a644d41
ipalv9 <ipal7271@gmail.com>
2024-11-09 10:44:03 +0700

    membuat koneksi

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "quiz1_asfalasfia";
+
+$koneksi = mysqli_connect($host,$username,$password,$database);
+
+
+
+?>
---
+++ proses.php
@@ -0,0 +1,12 @@
+<?php
+
+include("koneksi");
+
+
+
+
+
+
+
+
+?>
